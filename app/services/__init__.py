742f933e6e7535dab4436f762bf3e96477b2124e
Ayush-div <ayushdivedi797@gmail.com>
2025-11-19 19:18:21 +0530

    created separate files for functions used in main.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -3,6 +3,10 @@ from app.services.script_generator import *
 from app.services.generate_voice import *
 from app.services.generate_image import *
 from app.services.generate_media import *
+from app.services.resize_and_center import *
+from app.services.apply_pan_effect import *
+from app.services.add_multiple_texts import *
+from app.services.concatenate_media import *
 
 __all__ = [
     generate_prompts_from_prompt,
@@ -10,4 +14,8 @@ __all__ = [
     generate_voice_from_segments,
     generate_image_from_prompt,
     generate_media_sequence,
+    apply_pan_effect,
+    concatenate_media,
+    add_multiple_texts,
+    resize_and_center,
 ]
